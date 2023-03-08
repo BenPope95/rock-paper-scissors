d5e3065fbcf644fd1f46def27123b96ad64609e7
BenPope95 <121143325+BenPope95@users.noreply.github.com>
2023-03-07 20:35:57 -0500

    Fixed round and win countwhen round ends. Project is finished and playable.

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function playRound(playerChoice, computerChoice) {
     let round = 0
     let playerWins = 0
     let computerWins = 0
-    while (playerWins < 3 || computerWins < 3) {
+    let game = true
+    while (game) {
         let computerChoice = getComputerChoice();
         let playerChoice = prompt("select a move\nrock\npaper\nscissors")
         if (playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "scissors" && computerChoice === "paper" || playerChoice === "paper" && computerChoice === "rock") {
@@ -36,11 +37,27 @@ function playRound(playerChoice, computerChoice) {
             round++
             alert(`computer chose ${computerChoice}. You lost round ${round}.\nWins: ${playerWins}\nLosses: ${computerWins}`)
         }
-        if (playerWins === 5) {
+        if (playerWins === 3) {
             alert("Congratulations! You won!")
+            game = false
+            let playAgain = prompt("Play another round?\nyes/no")
+            if (playAgain === "yes") {
+                game = true
+                round = 0
+                computerWins = 0
+                playerWins = 0
+            }
         } 
-        if (computerWins === 5) {
+        if (computerWins === 3) {
             alert("You Lost. Try another round.")
+            game = false
+            let playAgain = prompt("Play another round?\nyes/no")
+            if (playAgain === "yes") {
+                game = true
+                round = 0
+                computerWins = 0
+                playerWins = 0
+            }
         }
     
     }
